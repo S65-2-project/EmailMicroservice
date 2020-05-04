c54ed516196e80f14c1c97e74587100d72a64a51
Davy de Haas <davy.dehaas98@gmail.com>
2020-05-04 10:36:07 +0200

    Add .gitignore and fix folder structure

---
+++ EmailMicroservice/Services/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace EmailMicroservice.Services
+{
+    public interface IEmailService
+    {
+        Task SendWelcomeEmail(string emailAddress);
+        Task SendEmail(string emailAddress, int emailType);
+    }
+}
