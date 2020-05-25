8f224ca67c8684115506e8205312f1c254953903
Davy de Haas <davy.dehaas98@gmail.com>
2020-05-25 10:07:33 +0200

    Revert "Feature/s65 154 (#3)"
    This reverts commit 2b5d65aa59767f197babb7863c9d2501a131cba9.

--- EmailMicroservice/Helper/EmailGenerator.cs
+++ EmailMicroservice/Helper/EmailGenerator.cs
@@ -9,7 +9,7 @@ namespace EmailMicroservice.Helper
             {
                 Subject = "Welcome to the Lisk Marketplace!",
                 Body =
-                    "<h1><span>Welcome to the Lisk Marketplace!</span></h1>\n<h2 style=\"color: #2e6c80;\"><span style=\"color: #000000;\">You have succesfully created an account on our platform! We hope you have tons of fun.</span></h2>\n<p>&nbsp;</p>"
+                    "style=\"color: #000000;\">Welcome to the Lisk Marketplace!</span></h1>\n<h2 style=\"color: #2e6c80;\"><span style=\"color: #000000;\">You have succesfully created an account on our platform! We hope you have tons of fun.</span></h2>\n<p>&nbsp;</p>"
             };
             return message;
         }
